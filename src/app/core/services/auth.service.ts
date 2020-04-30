da0f7e4f009d58e146b208ace6ce4aed37bf0a6b
Caubinho Pacheco <caubinho@gmail.com>
2020-04-30 13:23:57 +0200

    Service Auth

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { auth } from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afAuht: AngularFireAuth) { }
+
+  private signInWithEmail({ email, password }): Promise<auth.UserCredential> {
+    return this.afAuht.auth.signInWithEmailAndPassword(email, password);
+  }
+}
