12159a2f0db2dcc089142bc061a5bc3aaec6939c
Caubinho Pacheco <caubinho@gmail.com>
2020-04-30 13:30:33 +0200

    SignIn - signUp

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -12,4 +12,14 @@ export class AuthService {
   private signInWithEmail({ email, password }): Promise<auth.UserCredential> {
     return this.afAuht.auth.signInWithEmailAndPassword(email, password);
   }
+
+  private signUpWithEmail({ email, password, name }): Promise<auth.UserCredential> {
+    return this.afAuht.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(credencials => 
+        credencials.user
+        .updateProfile({ displayName: name, photoURL: null})
+        .then(() => credencials)
+        )
+  }
 }
