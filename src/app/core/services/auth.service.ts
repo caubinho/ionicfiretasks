317dd5f120a7e9626e91018f25a32e1cf14a899a
Caubinho Pacheco <caubinho@gmail.com>
2020-04-30 15:04:57 +0200

    Auth Types

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { User, AuthProvider } from './auth.types';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
 import { auth } from 'firebase/app';
@@ -9,11 +10,11 @@ export class AuthService {
 
   constructor(private afAuht: AngularFireAuth) { }
 
-  private signInWithEmail({ email, password }): Promise<auth.UserCredential> {
+  private signInWithEmail({ email, password }: User): Promise<auth.UserCredential> {
     return this.afAuht.auth.signInWithEmailAndPassword(email, password);
   }
 
-  private signUpWithEmail({ email, password, name }): Promise<auth.UserCredential> {
+  private signUpWithEmail({ email, password, name }: User): Promise<auth.UserCredential> {
     return this.afAuht.auth
       .createUserWithEmailAndPassword(email, password)
       .then(credencials => 
@@ -21,5 +22,19 @@ export class AuthService {
         .updateProfile({ displayName: name, photoURL: null})
         .then(() => credencials)
         )
+  }
+
+  private signInWithPopup(provider: AuthProvider): Promise<auth.UserCredential> {
+    let signInProvider = null;
+
+    switch(provider){
+      case AuthProvider.Facebook:
+        signInProvider = new auth.FacebookAuthProvider();
+      break;
+    }
+
+    return this.afAuht.auth.signInWithPopup(signInProvider);
+
+
   }
 }
